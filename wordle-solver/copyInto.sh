f0c64a6d606ba9a4beac0f76f9190a221cf43fda
Jonathan Olson <jonathan.olson@colorado.edu>
2022-01-18 01:30:33 -0700

    Website update with initial wordle page. Removed disqus. Changed configs.

---
+++ wordle-solver/copyInto.sh
@@ -0,0 +1,3 @@
+cp ../../wordle-solver/data/* data/
+cp ../../wordle-solver/lib/* lib/
+cp ../../wordle-solver/index.html index.html
---
+++ wordle-solver/data/copyInData.sh
@@ -0,0 +1,25 @@
+
+echo -n "export default " > crane.tree.total.js
+cat ../../miscworks/computed/wordle/crane.tree.total.json >> crane.tree.total.js
+
+echo -n "export default " > crate.tree.total.js
+cat ../../miscworks/computed/wordle/crate.tree.total.json >> crate.tree.total.js
+
+echo -n "export default " > slate.tree.total.js
+cat ../../miscworks/computed/wordle/slate.tree.total.json >> slate.tree.total.js
+
+echo -n "export default " > trace.tree.total.js
+cat ../../miscworks/computed/wordle/trace.tree.total.json >> trace.tree.total.js
+
+echo -n "export default " > alter.tree.js
+cat ../../miscworks/computed/wordle/alter.tree.json >> alter.tree.js
+
+echo -n "export default " > lance.tree.js
+cat ../../miscworks/computed/wordle/lance.tree.json >> lance.tree.js
+
+echo -n "export default " > rance.tree.js
+cat ../../miscworks/computed/wordle/rance.tree.json >> rance.tree.js
+
+echo -n "export default " > rated.tree.js
+cat ../../miscworks/computed/wordle/rated.tree.json >> rated.tree.js
+
